f78c5f7abf53665fd038f604ad7cb306bca2e511
Anh <anhlevietexe@gmail.com>
2019-11-10 19:19:02 +0800

    split start docker to a different file

---
+++ src/container.rs
@@ -0,0 +1,66 @@
+use std::env;
+use tokio::prelude::{Future, Stream};
+
+pub fn start_docker(config: &yaml_rust::Yaml, docker: &shiplift::Docker) {
+    // pull the image
+    let image_name = config["environment"]["image"].as_str().unwrap();
+
+    let container_name = get_image_name();
+    let img = shiplift::PullOptions::builder().image(image_name).build();
+    let fut = docker
+        .images()
+        .pull(&img)
+        .for_each(|output| {
+            if output["id"] != serde_json::Value::Null {
+                print!("{}", output["id"].as_str().unwrap());
+            }
+            if output["status"] != serde_json::Value::Null {
+                print!(" {}", output["status"].as_str().unwrap());
+            }
+            if output["progress"] != serde_json::Value::Null {
+                print!(" {}", output["progress"].as_str().unwrap());
+            }
+            println!("");
+            Ok(())
+        })
+        .map_err(|e| eprintln!("Error: {}", e));
+    tokio::run(fut);
+
+    // check if container already exist, else create
+    let container = docker.containers().get(&container_name);
+    let del_fut = container.delete().map_err(|e| eprintln!("Error: {}", e));
+    tokio::run(del_fut);
+    println!("removed existing container {:?}", container.id());
+
+    // create the container
+    let fut = docker
+        .containers()
+        .create(
+            &shiplift::ContainerOptions::builder(image_name.as_ref())
+                .name(&container_name)
+                .build(),
+        )
+        .map(|info| println!("{:?}", info))
+        .map_err(|e| eprintln!("Error: {}", e));
+    tokio::run(fut);
+}
+
+fn get_image_name() -> String {
+    // generates the image name
+    let path = env::current_dir();
+    let path = match path {
+        Ok(dir) => dir,
+        Err(e) => panic!("Problem accessing the current directory : {:?}", e),
+    };
+    let dir = path.file_name();
+    let dir = match dir {
+        Some(d) => match d.to_str() {
+            Some(t) => t.to_string(),
+            None => String::from(""),
+        },
+        None => String::from(""),
+    };
+    let mut container_name = String::from("carboncopy_");
+    container_name.push_str(&dir);
+    container_name
+}
--- src/main.rs
+++ src/main.rs
@@ -2,73 +2,13 @@ extern crate serde_json;
 extern crate shiplift;
 extern crate tokio;
 extern crate yaml_rust;
-use std::env;
-use tokio::prelude::{Future, Stream};
 mod config;
+mod container;
 
 fn main() {
     // read Carbonfile
     let config = config::get_config();
     let docker = shiplift::Docker::new();
 
-    start_docker(&config, &docker);
-}
-
-fn start_docker(config: &yaml_rust::Yaml, docker: &shiplift::Docker) {
-    // pull the image
-    let image_name = config["environment"]["image"].as_str().unwrap();
-
-    // naming convention for docker container name
-    let path = env::current_dir();
-    let path = match path {
-        Ok(dir) => dir,
-        Err(e) => panic!("Problem accessing the current directory : {:?}", e),
-    };
-    let dir = path.file_name();
-    let dir = match dir {
-        Some(d) => match d.to_str() {
-            Some(t) => t.to_string(),
-            None => String::from(""),
-        },
-        None => String::from(""),
-    };
-    let mut container_name = String::from("carboncopy_");
-    container_name.push_str(&dir);
-    let img = shiplift::PullOptions::builder().image(image_name).build();
-    let fut = docker
-        .images()
-        .pull(&img)
-        .for_each(|output| {
-            if output["id"] != serde_json::Value::Null {
-                print!("{}", output["id"].as_str().unwrap());
-            }
-            if output["status"] != serde_json::Value::Null {
-                print!(" {}", output["status"].as_str().unwrap());
-            }
-            if output["progress"] != serde_json::Value::Null {
-                print!(" {}", output["progress"].as_str().unwrap());
-            }
-            println!("");
-            Ok(())
-        })
-        .map_err(|e| eprintln!("Error: {}", e));
-    tokio::run(fut);
-
-    // check if container already exist, else create
-    let container = docker.containers().get(&container_name);
-    let del_fut = container.delete().map_err(|e| eprintln!("Error: {}", e));
-    tokio::run(del_fut);
-    println!("removed existing container {:?}", container.id());
-
-    // create the container
-    let fut = docker
-        .containers()
-        .create(
-            &shiplift::ContainerOptions::builder(image_name.as_ref())
-                .name(&container_name)
-                .build(),
-        )
-        .map(|info| println!("{:?}", info))
-        .map_err(|e| eprintln!("Error: {}", e));
-    tokio::run(fut);
+    container::start_docker(&config, &docker);
 }
